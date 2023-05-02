ae35171cb0af3ab42264885d1a2140bf5b06b226
FokiDoki <23121394+FokiDoki@users.noreply.github.com>
2023-05-02 15:38:08 +0300

    Initial commit

---
+++ src/main/java/com/wireguard/WireguardControllerApplication.java
@@ -0,0 +1,13 @@
+package com.wireguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WireguardControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WireguardControllerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wireguard/WireguardControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wireguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WireguardControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
