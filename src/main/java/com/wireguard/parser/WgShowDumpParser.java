924d4678201915491cfcbce7533cd7124c8c913a
FokiDoki <23121394+FokiDoki@users.noreply.github.com>
2023-05-03 19:52:45 +0300

    added logging

--- src/main/java/com/wireguard/parser/WgShowDumpParser.java
+++ src/main/java/com/wireguard/parser/WgShowDumpParser.java
@@ -3,7 +3,10 @@ package com.wireguard.parser;
 import com.wireguard.DTO.WgInterface;
 import com.wireguard.DTO.WgPeer;
 import com.wireguard.DTO.WgShowDump;
+import com.wireguard.external.UbuntuCommandExecutor;
 import lombok.Data;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.Assert;
 
 import java.io.BufferedReader;
@@ -15,6 +18,7 @@ import java.util.function.Function;
 @Data
 public class WgShowDumpParser {
 
+    private static final Logger logger = LoggerFactory.getLogger(WgShowDumpParser.class);
 
     private static final String SPLITTER = "\t";
     private static final Function<String, List<String>> splitAndReplaceOffs =
@@ -23,6 +27,7 @@ public class WgShowDumpParser {
     }
 
     public static WgShowDump fromDump(BufferedReader dump) throws IOException {
+        logger.trace("Parsing wg show dump");
         WgInterface wgInterface = parseInterface(dump);
         List<WgPeer> peers = parsePeers(dump);
         return new WgShowDump(wgInterface, peers);
@@ -32,6 +37,7 @@ public class WgShowDumpParser {
         List<WgPeer> peers = new ArrayList<WgPeer>();
         String line;
         while ((line = peersDump.readLine()) != null && !line.isEmpty()) {
+            logger.trace("Parsing peer "+line);
             peers.add(WgPeerParser.parse(splitAndReplaceOffs.apply(line)));
         }
         return peers;
@@ -39,6 +45,7 @@ public class WgShowDumpParser {
 
     private static WgInterface parseInterface(BufferedReader interfaceDump) throws IOException {
         String line = interfaceDump.readLine();
+        logger.trace("Parsing interface "+line);
         Assert.notNull(line, "Wg conf header is empty, invalid config");
         return WgInterfaceParser.parse(splitAndReplaceOffs.apply(line));
     }
