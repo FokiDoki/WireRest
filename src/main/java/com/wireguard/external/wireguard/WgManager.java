0c73d3b30bae10ddac6a5ed91962c95dd91d2ae0
FokiDoki <23121394+FokiDoki@users.noreply.github.com>
2023-05-19 18:46:42 +0300

    Added fake wgTool to interact with api witout real interface

--- src/main/java/com/wireguard/external/wireguard/WgManager.java
+++ src/main/java/com/wireguard/external/wireguard/WgManager.java
@@ -1,6 +1,7 @@
 package com.wireguard.external.wireguard;
 
 import com.wireguard.external.shell.ShellRunner;
+import com.wireguard.external.wireguard.dto.WgInterface;
 import com.wireguard.external.wireguard.dto.WgPeer;
 import com.wireguard.external.wireguard.dto.WgShowDump;
 import org.slf4j.Logger;
@@ -12,23 +13,28 @@ import java.io.IOException;
 import java.util.List;
 
 @Component
-public class WgInterfaceController {
+public class WgManager {
 
     private static final Logger logger = LoggerFactory.getLogger(ShellRunner.class);
     private String interfaceName = "wg0";
     private static WgTool wgTool;
 
     @Autowired
-    public WgInterfaceController(WgTool wgTool) {
-        WgInterfaceController.wgTool = wgTool;
+    public WgManager(WgTool wgTool) {
+        WgManager.wgTool = wgTool;
     }
 
 
 
 
-    public List<WgPeer> getAllPeers() throws IOException {
-        WgShowDump wgShowDump = wgTool.showDump(interfaceName);
-        return wgShowDump.getPeers();
+    public WgInterface getInterface() {
+        try{
+            WgShowDump wgShowDump = wgTool.showDump(interfaceName);
+            return wgShowDump.getWgInterface();
+        } catch (IOException e) {
+            logger.error("Error getting dump", e);
+            return null;
+        }
     }
 
 
---
+++ src/main/java/com/wireguard/external/wireguard/test/FakeWgTool.java
@@ -0,0 +1,37 @@
+package com.wireguard.external.wireguard.test;
+
+import com.wireguard.converters.StreamToStringConverter;
+import com.wireguard.external.wireguard.WgTool;
+import com.wireguard.external.wireguard.dto.WgShowDump;
+import com.wireguard.parser.WgShowDumpParser;
+import lombok.SneakyThrows;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.stream.Stream;
+
+@Profile("test")
+@Component
+public class FakeWgTool extends WgTool {
+
+    private final static StreamToStringConverter streamToStringConverter = new StreamToStringConverter();
+    @SneakyThrows
+    @Override
+    public WgShowDump showDump(String interfaceName) {
+        File wgShowDumpFile = new File("src/main/resources/test-data/wg_show_dump.txt");
+        String dump = streamToStringConverter.convert(new FileInputStream(wgShowDumpFile));
+        return WgShowDumpParser.fromDump(dump);
+    }
+
+    @Override
+    public String generatePrivateKey() {
+        return null;
+    }
+
+    @Override
+    public String generatePublicKey(String privateKey) {
+        return null;
+    }
+}
