5517c845cff7716f8f56707310ae49ea94ce2d17
FokiDoki <23121394+FokiDoki@users.noreply.github.com>
2023-05-19 18:27:14 +0300

    Added openAPI support and test api endpoint

--- src/main/java/com/wireguard/api/controllers/PeersAPIController.java
+++ src/main/java/com/wireguard/api/controllers/PeersAPIController.java
@@ -1,8 +1,27 @@
 package com.wireguard.api.controllers;
 
+import com.wireguard.external.wireguard.dto.WgInterface;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springdoc.core.annotations.RouterOperation;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class PeersAPIController {
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = WgInterface.class)) }),
+            @ApiResponse(responseCode = "404", description = "No interface found",
+                    content = @Content) })
+    @GetMapping("/interface")
+    public WgInterface getInterface() {
+        return new WgInterface("privateKey", "publicKey", 1234, 0);
+    }
 }
--- src/main/java/com/wireguard/external/wireguard/dto/WgInterface.java
+++ src/main/java/com/wireguard/external/wireguard/dto/WgInterface.java
@@ -1,5 +1,7 @@
 package com.wireguard.external.wireguard.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,6 +10,8 @@ import lombok.Data;
 public class WgInterface {
     private String privateKey;
     private String publicKey;
+    @Min(1)
+    @Max(65535)
     private int listenPort;
     private int fwmark;
 
