d2be6bf4b1346091403e43f5ba785b79f007bad2
FokiDoki <23121394+FokiDoki@users.noreply.github.com>
2023-05-20 15:18:20 +0300

    Tests for parser and bugfix

--- src/main/java/com/wireguard/parser/Utils.java
+++ src/main/java/com/wireguard/parser/Utils.java
@@ -4,8 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Utils {
-    private static final String BETWEEN_DATA_SPLITTER = "\t";
-    private static final String BASE64_REGEX = "^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$";
+    private static final String BASE64_REGEX = "^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$";
 
     public static ArrayList<String> trimAll(List<String> args){
         ArrayList<String> trimmedArgs = new ArrayList<>();
@@ -13,14 +12,6 @@ public class Utils {
             trimmedArgs.add(arg.trim());
         }
         return trimmedArgs;
-    }
-
-    public static List<String> splitLine(String arg){
-        return List.of(arg.split(BETWEEN_DATA_SPLITTER));
-    }
-
-    public static String replaceOffsWithZero(String arg){
-        return arg.replaceAll("off", "0");
     }
 
     public static Boolean isBase64(String arg){
--- src/test/java/com/wireguard/parser/WgInterfaceParserTests.java
+++ src/test/java/com/wireguard/parser/WgInterfaceParserTests.java
@@ -1,4 +1,83 @@
+package com.wireguard.parser;
+
+import com.wireguard.external.wireguard.dto.WgInterface;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class WgInterfaceParserTests {
-  
+
+    private final String validbase64 = "ZHdjdmRlZXd2ZXd2ZXd2ZXd2ZXdibGVyYm1yLGJlcg==";
+    @Test
+    void emptyStringParsingTest() {
+        String emptyString = "";
+        assertThrows(IllegalArgumentException.class,
+                () -> WgInterfaceParser.parse(emptyString, "\n"));
+    }
+
+    @Test
+    void overloadedStringParsingTest() {
+        String overloadedString = "1 2 3 4 5 6 7 8 9 10 11 12 13 14";
+        assertThrows(IllegalArgumentException.class,
+                () -> WgInterfaceParser.parse(overloadedString, "\n"));
+    }
+
+    @Test
+    void emptyListParsingTest() {
+        assertThrows(IllegalArgumentException.class,
+                () -> WgInterfaceParser.parse(List.of()));
+    }
+
+    @Test
+    void invalidBase64ParsingTest(){
+        String invalid2keys = "1234567890\t1234567890\t1234\t1234\n";
+        assertThrows(NotABase64Exception.class, () -> WgInterfaceParser.parse(invalid2keys, "\t"));
+        String validFirstBase64ButInvalidTwo = "%s\t1234567890\t1234\t1234\n".formatted(validbase64);
+        assertThrows(NotABase64Exception.class, () -> WgInterfaceParser.parse(validFirstBase64ButInvalidTwo, "\t"));
+    }
+
+    @Test
+    void invalidPortParsingTest(){
+        String invalidPort = "%s\t%s\t0\t1234\n".formatted(validbase64,validbase64);
+        assertThrows(IllegalArgumentException.class, () -> WgInterfaceParser.parse(invalidPort, "\t"));
+    }
+
+    @Test
+    void invalidPortParsingTest2(){
+        String invalidPort = "%s\t%s\t1234567890\t1234\n".formatted(validbase64,validbase64);
+        assertThrows(IllegalArgumentException.class, () -> WgInterfaceParser.parse(invalidPort, "\t"));
+    }
+
+    @Test
+    void noPortParsingTest(){
+        String noPort = "%s\t%s\t \t1234\n".formatted(validbase64,validbase64);
+        assertThrows(IllegalArgumentException.class, () -> WgInterfaceParser.parse(noPort, "\t"));
+    }
+
+    @Test
+    void validStringParsingTest(){
+        String validString =   "%s\t%s\t1234\t321  ".formatted(validbase64,validbase64);
+        WgInterface wgInterface = WgInterfaceParser.parse(validString, "\t");
+        assertEquals(wgInterface.getPrivateKey(), validbase64);
+        assertEquals(wgInterface.getPublicKey(), validbase64);
+        assertEquals(wgInterface.getListenPort(), 1234);
+        assertEquals(wgInterface.getFwmark(), 321);
+    }
+
+    @Test
+    void validStringWithOffFwmark(){
+        String validString =   "%s\t%s\t64444\toff  ".formatted(validbase64,validbase64);
+        WgInterface wgInterface = WgInterfaceParser.parse(validString, "\t");
+        assertEquals(wgInterface.getPrivateKey(), validbase64);
+        assertEquals(wgInterface.getPublicKey(), validbase64);
+        assertEquals(wgInterface.getListenPort(), 64444);
+        assertEquals(wgInterface.getFwmark(), 0);
+    }
+
+
+    @Test
+    void testParse() {
+    }
 }
