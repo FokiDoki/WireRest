5adf38e1774c392d0dff88975f659f83b61f00c4
FokiDoki <23121394+FokiDoki@users.noreply.github.com>
2023-07-10 17:37:34 +0300

    IpResolver renamed to SubnetResolver and Queued

---
+++ src/main/java/com/wireguard/external/network/ISubnetSolver.java
@@ -0,0 +1,35 @@
+package com.wireguard.external.network;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.concurrent.Future;
+
+public interface ISubnetSolver {
+
+    Subnet obtainFree(int mask);
+
+    void obtain(Subnet subnet);
+
+    void obtainIp(String ip);
+
+    void release(Subnet subnet);
+
+    long getAvailableIpsCount();
+
+    long getTotalIpsCount();
+
+    long getUsedIpsCount();
+
+    @Data
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    class IpRange {
+        private long least;
+        private long biggest;
+        public long getIpsCount(){
+            return biggest - least + 1;
+        }
+    }
+
+}
---
+++ src/main/java/com/wireguard/external/network/QueuedSubnetSolver.java
@@ -0,0 +1,59 @@
+package com.wireguard.external.network;
+
+
+import lombok.SneakyThrows;
+
+import java.util.Queue;
+import java.util.concurrent.*;
+
+public class QueuedSubnetSolver implements ISubnetSolver{
+
+    private final ISubnetSolver subnetSolver;
+
+
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
+
+    public QueuedSubnetSolver(ISubnetSolver subnetSolver){
+        this.subnetSolver = subnetSolver;
+    }
+
+    @Override
+    public Subnet obtainFree(int mask) {
+        Future<Subnet> subnetFuture = executor.submit(() -> subnetSolver.obtainFree(mask));
+        try {
+            return subnetFuture.get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void obtain(Subnet subnet) {
+        executor.submit(() -> subnetSolver.obtain(subnet));
+    }
+
+    @Override
+    public void obtainIp(String ip) {
+        executor.submit(() -> subnetSolver.obtainIp(ip));
+    }
+
+    @Override
+    public void release(Subnet subnet) {
+        executor.submit(() -> subnetSolver.release(subnet));
+    }
+
+    @Override
+    public long getAvailableIpsCount() {
+        return subnetSolver.getAvailableIpsCount();
+    }
+
+    @Override
+    public long getTotalIpsCount() {
+        return subnetSolver.getTotalIpsCount();
+    }
+
+    @Override
+    public long getUsedIpsCount() {
+        return subnetSolver.getUsedIpsCount();
+    }
+}
