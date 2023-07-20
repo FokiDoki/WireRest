5d98e6bc01c07d298443ec0c87c525b14cc52d2e
FokiDoki <23121394+FokiDoki@users.noreply.github.com>
2023-07-20 18:21:11 +0300

    fake tool real keys

--- src/main/java/com/wireguard/external/wireguard/test/FakeWgTool.java
+++ src/main/java/com/wireguard/external/wireguard/test/FakeWgTool.java
@@ -15,24 +15,25 @@ import java.util.*;
 @Profile("test")
 @Component
 public class FakeWgTool extends WgTool {
-    private Map<String, WgPeer> peers = new HashMap<>();
-    private WgInterface wgInterface;
+    private final Map<String, WgPeer> peers = new HashMap<>();
+    private final WgInterface wgInterface;
+    private final Base64.Encoder base64Encoder = Base64.getEncoder();
     private int keyCounter;
 
     public FakeWgTool() {
         super(1);
         wgInterface = new WgInterface("privkey", "pubkey", 16666, 0);
         List.of(
-                WgPeer.publicKey("PubKey1").build(),
-                WgPeer.publicKey("PubKey2").presharedKey("presharedKey2").build(),
-                WgPeer.publicKey("PubKey3").presharedKey("presharedKey3").endpoint("10.0.0.1").build(),
-                WgPeer.publicKey("PubKey4").presharedKey("presharedKey4").allowedIPv4Subnets(Set.of(Subnet.valueOf("10.0.0.2/32"))).build(),
-                WgPeer.publicKey("PubKey5").presharedKey("presharedKey5").allowedIPv4Subnets(Set.of(Subnet.valueOf("10.0.0.3/32"))).build(),
-                WgPeer.publicKey("PubKey6").presharedKey("presharedKey6").allowedIPv4Subnets(
+                WgPeer.publicKey(genPubKey()).build(),
+                WgPeer.publicKey(genPubKey()).presharedKey(generatePresharedKey()).build(),
+                WgPeer.publicKey(genPubKey()).presharedKey(generatePresharedKey()).endpoint("10.0.0.1").build(),
+                WgPeer.publicKey(genPubKey()).presharedKey(generatePresharedKey()).allowedIPv4Subnets(Set.of(Subnet.valueOf("10.0.0.2/32"))).build(),
+                WgPeer.publicKey(genPubKey()).presharedKey(generatePresharedKey()).allowedIPv4Subnets(Set.of(Subnet.valueOf("10.0.0.3/32"))).build(),
+                WgPeer.publicKey(genPubKey()).presharedKey(generatePresharedKey()).allowedIPv4Subnets(
                         Set.of(Subnet.valueOf("10.0.0.4/32"),Subnet.valueOf("10.0.0.5/32"))).build(),
-                WgPeer.publicKey("PubKey7").presharedKey("presharedKey7").allowedIPv4Subnets(Set.of(Subnet.valueOf("10.0.0.6/32")))
+                WgPeer.publicKey(genPubKey()).presharedKey(generatePresharedKey()).allowedIPv4Subnets(Set.of(Subnet.valueOf("10.0.0.6/32")))
                         .latestHandshake(100000).transferRx(12345).transferTx(54321).build(),
-                WgPeer.publicKey("PubKey8").presharedKey("presharedKey8").allowedIps(Set.of(Subnet.valueOf("10.0.0.7/32"), SubnetV6.valueOf("::1/128")))
+                WgPeer.publicKey(genPubKey()).presharedKey(generatePresharedKey()).allowedIps(Set.of(Subnet.valueOf("10.0.0.7/32"), SubnetV6.valueOf("::1/128")))
                         .latestHandshake(200000).transferRx(12345).transferTx(54321).build()
         ).forEach(peer -> peers.put(peer.getPublicKey(), peer));
         keyCounter = peers.size()+1;
@@ -42,10 +43,14 @@ public class FakeWgTool extends WgTool {
     public WgShowDump showDump(String interfaceName) {
         return new WgShowDump(wgInterface, peers.values().stream().toList());
     }
+    private String genPubKey(){
+        return generatePublicKey(generatePrivateKey());
+    }
 
     @Override
     public String generatePrivateKey() {
-        return "PrivateKey"+keyCounter++;
+        String key = "FakePrvKeyFakePrvKey"+String.format("%11d", keyCounter++);
+        return base64Encoder.encodeToString(key.getBytes());
     }
 
     @SneakyThrows
@@ -69,12 +74,15 @@ public class FakeWgTool extends WgTool {
 
     @Override
     public String generatePublicKey(String privateKey) {
-        return "PubKey"+keyCounter++;
+        String key = "pub"+privateKey.substring(44-7)+"FakePubKey"+String.format("%11d", keyCounter++);
+        System.out.println("keypub: "+key);
+        return base64Encoder.encodeToString(key.getBytes());
     }
 
     @Override
     public String generatePresharedKey() {
-        return "preSharedKey"+keyCounter++;
+        String key = "FakePskKeyFakePskKey"+String.format("%11d", keyCounter++);
+        return base64Encoder.encodeToString(key.getBytes());
     }
 
 
