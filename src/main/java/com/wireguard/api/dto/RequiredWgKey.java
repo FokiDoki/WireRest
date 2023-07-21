8c7c07fa468d2eef52ad63dc545393dbacc0261e
FokiDoki <23121394+FokiDoki@users.noreply.github.com>
2023-07-21 15:21:07 +0300

    Fixed double error

--- src/main/java/com/wireguard/api/dto/RequiredWgKey.java
+++ src/main/java/com/wireguard/api/dto/RequiredWgKey.java
@@ -7,19 +7,18 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
 @Getter
+@Setter
+@AllArgsConstructor
 public class RequiredWgKey extends WgKey {
 
     @Size(min=44, max = 44, message = "Key must be 44 characters long")
     @Pattern(regexp = "^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)$",
-            message = "Invalid key format")
+            message = "Invalid key format (Base64 required)")
     private final String value;
-
-    public RequiredWgKey(String value) {
-        super(value);
-        this.value = value;
-    }
 }
