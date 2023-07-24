653216424a0987d01cda9fc0f9696eb1d4cf4777
FokiDoki <23121394+FokiDoki@users.noreply.github.com>
2023-07-24 16:55:13 +0300

    Examples customizer

---
+++ src/main/java/com/wireguard/api/ExamplesCustomizer.java
@@ -0,0 +1,8 @@
+package com.wireguard.api;
+
+import io.swagger.v3.oas.models.examples.Example;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExamplesCustomizer extends OpenAPIMapCustomizer<String, Example>{
+}
