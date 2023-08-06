2dedf114d09108f539fab068be33662610d233e2
FokiDoki <23121394+FokiDoki@users.noreply.github.com>
2023-08-06 15:19:27 +0300

    OpenAPI description

--- src/main/java/com/wirerest/api/service/StatsController.java
+++ src/main/java/com/wirerest/api/service/StatsController.java
@@ -1,9 +1,17 @@
 package com.wirerest.api.service;
 
+import com.wirerest.api.AppError;
 import com.wirerest.api.converters.StatsSnapshotToDtoConverter;
+import com.wirerest.logs.LoggingEventDto;
 import com.wirerest.logs.LogsDao;
 import com.wirerest.stats.StatsService;
 import com.wirerest.stats.StatsSnapshot;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,9 +28,33 @@ public class StatsController {
         this.statsService = statsService;
     }
 
+    @Operation(summary = "Get application statistics",
+            description = """
+                    Timestamp - Unix milliseconds timestamp of the snapshot
+                                        
+                    peers - number of peers in the interface
+                    
+                    totalV4Ips - number of IPv4 addresses (/32) in the interface
+                    
+                    freeV4Ips - number of addresses (/32) that are not in use
+                    
+                    transferTx - total transmitted bytes for all peers
+                    
+                    transferRx - total received bytes for all peers""",
+            tags = {"Service"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "OK",
+                            content = {@Content(mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = StatsSnapshotDto.class)),
+                                    examples = {
+                                            @ExampleObject(name = "Stats",
+                                                    ref = "#/components/examples/stats")
+                                    })})
+            })
     @GetMapping("/v1/service/stats")
     public StatsSnapshotDto getStats() {
         return statsSnapshotToDtoConverter.convert(statsService.snapshot());
     }
+
 
 }
