30bd3b50e3494db37271368ed4ad28c48f387d85
FokiDoki <23121394+FokiDoki@users.noreply.github.com>
2023-08-19 13:22:32 +0300

    Config

---
+++ src/main/java/com/wirerest/api/security/Role.java
@@ -0,0 +1,13 @@
+package com.wirerest.api.security;
+
+public class Role {
+    public final static String GET_ALL_PEERS = "GET_ALL_PEERS";
+    public final static String GET_PEER = "GET_PEER";
+    public final static String CREATE_PEER = "CREATE_PEER";
+    public final static String UPDATE_PEER = "UPDATE_PEER";
+    public final static String DELETE_PEER = "DELETE_PEER";
+    public final static String GET_STATS = "GET_STATS";
+    public final static String GET_LOGS = "GET_LOGS";
+    public final static String GET_INTERFACE = "GET_INTERFACE";
+    public final static String FULL_ACCESS = "FULL_ACCESS";
+}
---
+++ src/main/java/com/wirerest/api/security/SecurityConfig.java
@@ -0,0 +1,70 @@
+package com.wirerest.api.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.util.matcher.AndServerWebExchangeMatcher;
+import org.springframework.security.web.server.util.matcher.OrServerWebExchangeMatcher;
+import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher;
+
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    private final String INITIAL_TOKEN;
+
+
+    public SecurityConfig(
+            @Value("${security.token}") String initialToken
+    ){
+        this.INITIAL_TOKEN = initialToken;
+    }
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        UserDetails user = User
+                .withUsername("root")
+                .password(INITIAL_TOKEN)
+                .roles(Role.FULL_ACCESS)
+                .build();
+        return new MapReactiveUserDetailsService(user);
+    }
+
+    @Order(Ordered.LOWEST_PRECEDENCE)
+    @Bean
+    SecurityWebFilterChain apiHttpSecurity(ServerHttpSecurity http) {
+        http
+                .authorizeExchange((exchanges) -> exchanges
+                        .anyExchange().authenticated()
+                );
+        return http.build();
+    }
+
+    @Order(30)
+    @Bean
+    SecurityWebFilterChain swaggerSecurity(ServerHttpSecurity http) {
+        http
+                .securityMatcher(new OrServerWebExchangeMatcher(
+                        List.of(new PathPatternParserServerWebExchangeMatcher("/webjars/swagger-ui/**"),
+                                new PathPatternParserServerWebExchangeMatcher("/swagger-ui"),
+                                new PathPatternParserServerWebExchangeMatcher("/v3/api-docs/swagger-config"),
+                                new PathPatternParserServerWebExchangeMatcher("/v3/api-docs")
+                        )
+                    )
+                ).authorizeExchange((exchanges) -> exchanges.anyExchange().permitAll());
+        return http.build();
+    }
+
+}
