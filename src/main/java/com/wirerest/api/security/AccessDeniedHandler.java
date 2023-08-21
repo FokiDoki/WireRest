c2238b815131143b5150105028d2c27f440bd5eb
FokiDoki <23121394+FokiDoki@users.noreply.github.com>
2023-08-21 15:40:05 +0300

    Performance optimization

--- src/main/java/com/wirerest/api/security/AccessDeniedHandler.java
+++ src/main/java/com/wirerest/api/security/AccessDeniedHandler.java
@@ -1,6 +1,9 @@
 package com.wirerest.api.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.wirerest.api.AppError;
+import lombok.SneakyThrows;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.server.authorization.ServerAccessDeniedHandler;
 import org.springframework.web.server.ServerWebExchange;
@@ -8,9 +11,17 @@ import reactor.core.publisher.Mono;
 
 public class AccessDeniedHandler implements ServerAccessDeniedHandler {
 
+    private final AppError error = new InvalidTokenAppError();
+    private final byte[] errorJson;
+    @SneakyThrows
+    public AccessDeniedHandler() {
+        ObjectMapper jsonMapper = new JsonMapper();
+        errorJson = jsonMapper.writeValueAsBytes(error);
+        System.out.println("Created Json Error ");
+    }
     @Override
     public Mono<Void> handle(ServerWebExchange exchange, AccessDeniedException ex) {
-        AppError error = new InvalidTokenAppError();
-        return Util.setErrorToResponse(exchange, error);
+
+        return Util.setErrorToResponse(exchange, error.getCode(), errorJson);
     }
 }
--- src/main/java/com/wirerest/api/security/Util.java
+++ src/main/java/com/wirerest/api/security/Util.java
@@ -1,19 +1,15 @@
 package com.wirerest.api.security;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.json.JsonMapper;
-import com.wirerest.api.AppError;
 import lombok.SneakyThrows;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 public class Util {
     @SneakyThrows
-    public static Mono<Void> setErrorToResponse(ServerWebExchange exchange, AppError error){
-        ObjectMapper jsonMapper = new JsonMapper();
-        exchange.getResponse().setRawStatusCode(error.getCode());
+    public static Mono<Void> setErrorToResponse(ServerWebExchange exchange, int httpErrorCode, byte[] error){
+        exchange.getResponse().setRawStatusCode(httpErrorCode);
         exchange.getResponse().getHeaders().add("Content-Type", "application/json");
         return exchange.getResponse().writeWith(Mono.just(exchange.getResponse().bufferFactory()
-                .wrap(jsonMapper.writeValueAsBytes(error))));
+                .wrap(error)));
     }
 }
